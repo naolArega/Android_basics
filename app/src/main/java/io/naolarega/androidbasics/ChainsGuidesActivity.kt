40b424d8586ec94175b8030f238d41f42e288ecd
naolArega <na408498@gmail.com>
2021-04-27 16:28:18 +0300

    Made every experiment its own activity

---
+++ app/src/main/java/io/naolarega/androidbasics/ChainsGuidesActivity.kt
@@ -0,0 +1,11 @@
+package io.naolarega.androidbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChainsGuidesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chains_guides)
+    }
+}
