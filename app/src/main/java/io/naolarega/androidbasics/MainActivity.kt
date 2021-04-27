ab27dbc81afc8ad5b3e0179fc52d3b7315b7678d
naolArega <na408498@gmail.com>
2021-04-27 11:15:26 +0300

    Tried out android debugging with logcat

--- app/src/main/java/io/naolarega/androidbasics/MainActivity.kt
+++ app/src/main/java/io/naolarega/androidbasics/MainActivity.kt
@@ -2,11 +2,27 @@ package io.naolarega.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = listOf(9, 5, 2, 8, 1, 3)
+        sortList(list)
+        Log.d("MainActivity :", "printing a sorted list $list")
+    }
+
+    private fun sortList(list: List<Int>){
+        for(i in 0..list.size - 1){
+            for(j in 0..list.size - 2){
+                if (list[j] > list[j + 1]) {
+                    Collections.swap(list, j, j + 1)
+                }
+            }
+        }
     }
 
     override fun onStart() {
