b1fd82359d42fe3dc88e780c6930b6901ef352de
naolArega <na408498@gmail.com>
2021-04-30 20:18:19 +0300

    Implicit intents

---
+++ app/src/main/java/io/naolarega/androidbasics/ImplicitIntentActivity.kt
@@ -0,0 +1,31 @@
+package io.naolarega.androidbasics
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_implicit_intent.*
+
+class ImplicitIntentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_implicit_intent)
+
+        btnChooseImage.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, 0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 0){
+            val imageUri = data?.data
+            ivImageDisplay.setImageURI(imageUri)
+            Toast.makeText(this, "The uri for the image is $imageUri", Toast.LENGTH_LONG).show()
+        }
+    }
+}
